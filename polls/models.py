efc74bceaa89eb4a3ca9a0c38cb9eefeeaf022e3
ablancaa <88708431+ablancaa@users.noreply.github.com>
2024-11-01 22:10:36 +0100

    Update models.py

--- polls/models.py
+++ polls/models.py
@@ -2,18 +2,6 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
-
-#class Question(models.Model):
-#    question_text = models.CharField(max_length=200)
-#    pub_date = models.DateTimeField("date published")
-
-
-#class Choice(models.Model):
-#    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#    choice_text = models.CharField(max_length=200)
-#    votes = models.IntegerField(default=0)
-
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published", default=timezone.now)
@@ -24,14 +12,6 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
-class Persona(models.Model):
-    persona_nom = models.CharField(max_length=200)
-    persona_cognom1 = models.CharField(max_length=100)
-    persona_data_naixement = models.DateTimeField('Data de naixement')
-    
-    def __str__(self):
-        return self.persona_nom+" "+self.persona_cognom1
-
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -40,3 +20,12 @@ class Choice(models.Model):
     # ...
     def __str__(self):
         return self.choice_text
+
+
+class Persona(models.Model):
+    persona_nom = models.CharField(max_length=200)
+    persona_cognom1 = models.CharField(max_length=100)
+    persona_data_naixement = models.DateTimeField('Data de naixement')
+    
+    def __str__(self):
+        return self.persona_nom+" "+self.persona_cognom1
