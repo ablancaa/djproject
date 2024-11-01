293f8c3a51f060770eea53209ff3c38fa8892eca
ablancaa <88708431+ablancaa@users.noreply.github.com>
2024-11-01 22:09:47 +0100

    Update admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import Question
-from .models import Persona
 from .models import Choice
+from .models import Persona
 
 admin.site.register(Question)
-admin.site.register(Persona)
 admin.site.register(Choice)
+admin.site.register(Persona)
+
