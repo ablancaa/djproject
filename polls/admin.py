4c28793d9fb3445d0f44b8731c7222d26d08242e
Alberto <ablancaa@gmail.com>
2024-11-01 17:33:04 +0100

    segundo envio

--- polls/admin.py
+++ polls/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 
 from .models import Question
 from .models import Persona
+from .models import Choice
 
 admin.site.register(Question)
 admin.site.register(Persona)
+admin.site.register(Choice)
